ef5943633dd409b24e261cdde4c692417e90c831
brennoDinaroski <brenno.dinaroski@gmail.com>
2023-07-27 16:03:49 -0300

    first commit

---
+++ test.js
@@ -0,0 +1,10 @@
+import {apiProducts01} from './script.js'
+
+export const exportTest = 1
+export let value01 = 1
+
+export const value02 = 2
+
+console.log('test: ', value02)
+
+/* console.log('apiProducts in "test.js" from "script.js": ', apiProducts01) */
