415b632aac13a8de40fa0a9577416dadf9758fa1
brennoDinaroski <brenno.dinaroski@gmail.com>
2023-08-03 19:23:34 -0300

    trying to store the card selected in localStorage and avoid the repetition problem

--- script.js
+++ script.js
@@ -6,22 +6,23 @@ const searchButton = document.querySelector("[data-search-button]")
 let apiProductsArray = []
 let addToCartButton = []
 
-if(searchInput != null){
-searchInput.addEventListener("input", (valueSearched) => {
-    const searchInformation = valueSearched.target.value.toLowerCase()
+if (searchInput != null) {
+    searchInput.addEventListener("input", (valueSearched) => {
+        const searchInformation = valueSearched.target.value.toLowerCase()
 
-    searchButton.addEventListener("click", () =>
+        searchButton.addEventListener("click", () =>
 
-        apiProductsArray.forEach(product => {
-            let isVisible =
-                product.title.toLowerCase().includes(searchInformation)
-                || product.brand.toLowerCase().includes(searchInformation)
+            apiProductsArray.forEach(product => {
+                let isVisible =
+                    product.title.toLowerCase().includes(searchInformation)
+                    || product.brand.toLowerCase().includes(searchInformation)
 
-            product.element.classList.toggle("hide", !isVisible)
-        })
-    )
+                product.element.classList.toggle("hide", !isVisible)
+            })
+        )
 
-})} else {}
+    })
+} else { }
 
 
 
@@ -51,6 +52,11 @@ export async function apiConsuming(contentDiv, functionCreateProduct) {
 function createProductListItem(item, contentDiv) {
     const dataElement = document.createElement('div')
     const link = document.createElement('a')
+    const buttonAddToCart = document.createElement('button')
+
+    buttonAddToCart.innerHTML = 'Add to Cart'
+    buttonAddToCart.setAttribute('class', 'button__addToCart')
+    buttonAddToCart.setAttribute('data-id', `${item.id}`)
 
     link.href = `./productDetails.html?id=${item.id}`
     link.textContent = item.title
@@ -65,14 +71,30 @@ function createProductListItem(item, contentDiv) {
             <div class='product__brand' > ${item.brand} </div>                    
             <img class='product__image' src = "${item.images[0]}">                    
             <div class='product__price' > R$ ${item.price} </div>
-            <div class='product__description' > ${item.description} </div>
-            <button class='button__addToCart' data-button-addtocart=${item.id} id="${item.id}" >Add To Cart</button>
+            <div class='product__description' > ${item.description} </div>            
         `;
 
-    contentDiv.appendChild(dataElement)
+        contentDiv.appendChild(dataElement)
+        
+    dataElement.appendChild(buttonAddToCart)
+    const buttonArray = document.querySelectorAll('.button__addToCart')
+    /* buttonArray.forEach((button) => */
+    buttonArray.addEventListener('click', (event) => {
+            event.preventDefault();
+            const clickedButton = event.target
+            const id = clickedButton.getAttribute('data-id')
+
+            const existingIds = JSON.parse(localStorage.getItem('SelectedIds')) || [];
+
+            existingIds.push(id)        
+            localStorage.setItem('SelectedIds', JSON.stringify(existingIds))
+
+            /* window.location.href = "#" */
+            console.log('button clicked: ', id)
+        })
+    /* ) */
 
     
-
 
 
     return {
@@ -85,11 +107,4 @@ function createProductListItem(item, contentDiv) {
         element: dataElement
     }
 };
-
-let i = 0
-    addToCartButton = document.querySelector('.button__addToCart')
-    addToCartButton.addEventListener('click', addToCartClickListener)
-
-
-
 
