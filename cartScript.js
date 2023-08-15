19971eddc1d5d9a81b41bd4b4f728cf412478a00
brennoDinaroski <brenno.dinaroski@gmail.com>
2023-08-15 01:23:08 -0300

    add the initial code to cart part to click and role on through the page to appear the cart

---
+++ cartScript.js
@@ -0,0 +1,10 @@
+let cartImage = document.querySelector('.cart__image')
+let closeShoppingCart = document.querySelector('.closeShopping')
+
+cartImage.addEventListener('click', () => {
+    console.log("cartImage clicked")
+})
+
+closeShoppingCart.addEventListener('click', () => {
+    console.log("closeShoppingCart clicked")
+})
